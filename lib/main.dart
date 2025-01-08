14446babfd44204c4309068ca251b5ce09954315
sreeramis8888 <128227002+sreeramis8888@users.noreply.github.com>
2025-01-09 01:56:45 +0530

    push notification with bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pravasitax_flutter/mainPage_consultant.dart';
@@ -5,7 +6,10 @@ import 'package:pravasitax_flutter/src/core/theme/app_theme.dart';
 import 'package:pravasitax_flutter/src/interface/screens/login_pages/splash_screen.dart';
 import 'package:pravasitax_flutter/mainpage.dart';
 
-void main() {
+Future<void> main() async {  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(
+  
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
